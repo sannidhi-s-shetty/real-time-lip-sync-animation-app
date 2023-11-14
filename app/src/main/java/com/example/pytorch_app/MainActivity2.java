988420f4c9d7543a557bcade79fdc82ea6e00d39
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2023-11-15 00:45:37 +0530

    displayinh one image

--- app/src/main/java/com/example/pytorch_app/MainActivity2.java
+++ app/src/main/java/com/example/pytorch_app/MainActivity2.java
@@ -51,8 +51,8 @@ public class MainActivity2 extends AppCompatActivity {
     int inSize = 512;
 
     // Width and height of the output image
-    int width = 256;
-    int height = 256;
+    int width = 128;
+    int height = 128;
 
     public void createTempDirectories() {
 
@@ -86,7 +86,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         // Load in the model
         try {
-//            module = LiteModuleLoader.load(assetFilePath("imageGen.pt"));
+            module = LiteModuleLoader.load(assetFilePath("imageGen.pt"));
             decoder = LiteModuleLoader.load(assetFilePath("vgnetmodeloptimised1126.pt"));
         } catch (IOException e) {
             Log.e(TAG, "Unable to load model", e);
@@ -164,20 +164,68 @@ public class MainActivity2 extends AppCompatActivity {
                         // [R, G, B, R, G, B, ..., B] and we
                         // have to deal with that.
 //                        float[] outputArr  = {0.1F};
-//                        float[] outputArr = module.forward(IValue.from(inputTensor)).toTensor().getDataAsFloatArray();
+//                        Tensor outputArr = module.forward(IValue.from(inputTensor)).toTensor();
+//                        Log.e(TAG,"outputArr: "+outputArr);
                         Log.e(TAG,"ok : started");
 //                        .toTensor().getDataAsDoubleArray()
                         try{
+                            long startTime = System.currentTimeMillis();
 
-                            IValue fake_ims = decoder.forward(IValue.from(finalExample_image), IValue.from(finalFake_lmark), IValue.from(finalExample_landmark));
-                            IValue[] fake_ims_tuple = fake_ims.toTuple();
-                            float[] outputArr = fake_ims_tuple[0].toTensor().getDataAsFloatArray();
+                            IValue output = decoder.forward(IValue.from(finalExample_image), IValue.from(finalFake_lmark), IValue.from(finalExample_landmark));
+                            long endTime = System.currentTimeMillis();
+                            long elapsedTime = endTime - startTime;
+
+                            Log.d("ProcessingSpeed", "Elapsed Time: " + elapsedTime + " milliseconds");
+                            IValue[] output_tuple = output.toTuple();
+                            Tensor fake_ims = output_tuple[0].toTensor();
+                            Log.e(TAG, "Type fake_ims : "+ fake_ims);
+//                            Log.e(TAG, "Length fake_ims : "+ fake_ims.length);
+                            float[] outputData = fake_ims.getDataAsFloatArray();
+                            long[] shape = fake_ims.shape();
+//                            Log.e(TAG, "outputData : "+ outputData.length);
+//                            Log.e(TAG, "shape : "+ shape[0]);
+//                            Log.e(TAG, "shape : "+ shape[1]);
+//                            Log.e(TAG, "shape : "+ shape[2]);
+
+                            for (int indx = 0; indx < 1; indx++) {
+                                float[] fakeStore = new float[(int) (shape[2] * shape[3] * shape[4])];//[1,149,3,128,128]
+                                System.arraycopy(outputData, indx * fakeStore.length, fakeStore, 0, fakeStore.length);
+                                long[] reshapeDims = {(int) shape[2], (int) shape[3], (int) shape[4]};
+                                Tensor reshapedTensor = Tensor.fromBlob(fakeStore, reshapeDims);
+
+                                int dim0 = 3;
+                                int dim1 = 128;
+                                int dim2 = 128;
+                                float[] newFakeStore = new float[128*128*3];
+                                for (int i = 0; i < dim0; i++) {
+                                    for (int j = 0; j < dim1; j++) {
+                                        for (int k = 0; k < dim2; k++) {
+                                            int indexOriginal = i * dim1 * dim2 + j * dim2 + k;
+                                            int indexNew = j * dim2 * dim0 + k * dim0 + i;
+                                            newFakeStore[indexNew] = fakeStore[indexOriginal];
+                                        }
+                                    }
+                                }
+//                                Log.e(TAG, "reshapedTensor : "+reshapedTensor);
+
+//                                float[] reshapedArray = reshapedTensor.getDataAsFloatArray();
+                                convertToBitmap(newFakeStore);
+                            }
+
                         }  catch (Error e) {
                             Log.e(TAG, "OMG : ", e);
                         }
 
 //                        float[] fake_ims, atts ,ms ,extra = decoder.forward(IValue.from(finalExample_image),IValue.from(finalFake_lmark),IValue.from(finalExample_landmark)).toTensor().getDataAsFloatArray();
                         Log.e(TAG,"hurray done");
+
+
+
+
+
+
+
+
 //                        Tensor  fake_lmar = module1.forward(IValue.from(example_landmark, input_mfcc)).toTensor();
                         // Ensure the output array has values between 0 and 255
 //                        for (int i = 0; i < outputArr.length; i++) {
@@ -224,6 +272,51 @@ public class MainActivity2 extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void convertToBitmap(float[] outputArr) {
+
+//         Ensure the output array has values between 0 and 255
+                        for (int i = 0; i < outputArr.length; i++) {
+
+                            outputArr[i] = Math.min(Math.max(outputArr[i], 0), 1)*255;
+                            Log.e(TAG, "outputArr : "+outputArr[i]);
+                        }
+
+//         Create a RGB bitmap of the correct shape
+                        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+                        // Iterate over all values in the output tensor
+                        // and put them into the bitmap
+                        int loc = 0;
+                        for (int y = 0; y < width; y++) {
+                            for (int x = 0; x < height; x++) {
+                                bmp.setPixel(x, y, Color.rgb((int)outputArr[loc], (int)outputArr[loc+1], (int)outputArr[loc+2]));
+                                loc += 3;
+                            }
+                        }
+
+//         The output of the network is no longer needed
+                        outputArr = null;
+
+                        // Resize the bitmap to a larger image
+                        bmp = Bitmap.createScaledBitmap(
+                                bmp, 512, 512, false);
+
+                        // Display the image
+                        Bitmap finalBmp = bmp;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ivImage.setImageBitmap(finalBmp);
+
+                                // Error handing
+                                btnGenerate.setClickable(true);
+                                tvWaiting.setVisibility(View.INVISIBLE);
+                                ivImage.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        );
     }
 
     // Generate a tensor of random doubles given the size of
@@ -299,4 +392,6 @@ public class MainActivity2 extends AppCompatActivity {
 //        Log.e(TAG,"bos.shape: "+bos.toString().length());
 //        return bos.toByteArray();
     }
+
+
 }
